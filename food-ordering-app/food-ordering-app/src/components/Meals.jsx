30cdcece730ef8d44aca71a1d6d4a09a51505811
dhruvi6071 <dhruvi6071@gmail.com>
2024-03-19 19:07:45 +0530

    stop-infinite-loo-from-async-await-by-useEffect

---
+++ food-ordering-app/food-ordering-app/src/components/Meals.jsx
@@ -0,0 +1,30 @@
+import { useState, useEffect } from "react";
+
+export default function Meals() {
+  const [loadMeals, setLoadMeals] = useState([]);
+  //If we got no dependencies or if we got dependencies that never change, this code will never execute again and therefore, we won't have an infinite loop.
+  
+  useEffect(() => {
+    //This function is responsible for making an asynchronous request to a server to get meal data.
+    
+    async function fetchMeals() {
+        const response = await fetch('http://localhost:3000/meals'); //To send an http request
+
+      if (!response.ok) {
+        console.log("There is an error.");
+      }
+      const meals = await response.json(); //it tries to parse the response body
+      setLoadMeals(meals); //To set loaded meal equal to meals gotten from backend.
+    }
+
+    fetchMeals();
+  }, []);
+
+  return (
+    <ul id="meals">
+      {loadMeals.map((meal) => (
+        <li key={meal.id}>{meal.name}</li>
+      ))}
+    </ul>
+  );
+}
