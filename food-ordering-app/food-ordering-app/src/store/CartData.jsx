66b00cbe00181e9be23518da47b1d14927789d51
dhruvi6071 <dhruvi6071@gmail.com>
2024-03-21 16:11:17 +0530

    add-to-cart-functionality

--- food-ordering-app/food-ordering-app/src/store/CartData.jsx
+++ food-ordering-app/food-ordering-app/src/store/CartData.jsx
@@ -11,6 +11,22 @@ const CartContext = createContext({
 function cartReducer(state, action) {
     if(action.type === 'ADD_ITEM') {
         //Update the state to add meal to the cart.
+        // state.item.push(action.item); //With this line we have like 10 times like to that item therefore use another way.
+        const existingCartItemIndex = state.items.findIndex((item) => item.id === action.item.id);
+    
+        const updatedItem = [...state.item];
+        if(existingCartItemIndex > -1){
+            const existingItem = state.item[existingCartItemIndex];
+            const updatedItem = {
+                ...existingItem,
+                quantity: existingItem.quantity + 1
+            };
+            updatedItem[existingCartItemIndex] = updatedItem;
+        } else{
+           updatedItem.push({ ...action.item, quantity: 1}); 
+        }
+
+        return {...state, items: updatedItem};
     }
 
     if(action === 'REMOVE_ITEM'){
@@ -22,7 +38,7 @@ function cartReducer(state, action) {
 
 //This function will do the actual data management and state management.
 function CartContextProvider({children}) {
-    useReducer() ; //This is also a hook that needed to manage the states but it convert complex task into comparatively simple.
+    useReducer(cartReducer, {items: []}) ; //This is also a hook that needed to manage the states but it convert complex task into comparatively simple.
     return <CartContext.Provider>{children}</CartContext.Provider> //This tag should be wrapped around any compoenent that need data of cartContext.
 }
 
