507bb1d575134be83a83f5c073b1b1f3d3687301
dhruvi6071 <dhruvi6071@gmail.com>
2024-03-23 16:02:38 +0530

    cart-number-shower

--- food-ordering-app/food-ordering-app/src/components/Header.jsx
+++ food-ordering-app/food-ordering-app/src/components/Header.jsx
@@ -1,6 +1,14 @@
 import image from '../assets/logo.jpg';
 import Button from './UI/Button';
+import CartContext from '../store/CartContext';
+import { useContext } from 'react';
 export default function Header() {
+
+    const CartCtx = useContext(CartContext);
+    const totalCartItems = CartCtx.items.reduce((totalNumberOfItem, item) => {
+        return totalNumberOfItem + item.quantity;
+    }, 0);
+
     return <header id='main-header'>
         <div id='title'>
             <img src={image} alt="logo" />
@@ -8,7 +16,7 @@ export default function Header() {
 
         </div>
         <nav>
-            <Button textOnly>Wish to Eat!</Button>
+            <Button textOnly>Wish to Eat!({totalCartItems})</Button>
         </nav>
     </header>
 }
