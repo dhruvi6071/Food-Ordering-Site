9d77c3ae5a164e240ec62c7d9f5daa8839992e46
dhruvi6071 <dhruvi6071@gmail.com>
2024-04-23 15:31:49 +0530

    close-cart

--- food-ordering-site/food-ordering-site/src/components/Cart.jsx
+++ food-ordering-site/food-ordering-site/src/components/Cart.jsx
@@ -9,11 +9,17 @@ import UserProgressContext from "./store/UserProgressContext";
 export default function Cart() {
   const cartCtx = useContext(CartContext);
   const userProgressCtx = useContext(UserProgressContext);
-  
+
   const cartTotal = cartCtx.items.reduce(
     (totalPrice, item) => totalPrice + item.quantity * item.price,
     0
   );
+
+ function handleCloseCart(){
+    userProgressCtx.hideCart();
+ }
+
+  
   return (
     <Modal className="cart" open={userProgressCtx.progress === 'cart'}>
       <h2>Your Wishlist</h2>
@@ -26,7 +32,7 @@ export default function Cart() {
       </ul>
       <p className="cart-total">{currencyFormatter.format(cartTotal)}</p>
       <p className="modal-actions">
-        <Button textOnly>Close</Button>
+        <Button textOnly onClick={handleCloseCart}>Close</Button>
         <Button >Go to CheckOut</Button>
       </p>
     </Modal>
